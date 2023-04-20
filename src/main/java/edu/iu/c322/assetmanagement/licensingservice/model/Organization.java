99e96bd9b090335f0d4e01238b52df352e93e881
nawhippo <93534375+nawhippo@users.noreply.github.com>
2023-04-19 20:05:03 -0400

    Delete src directory

--- src/main/java/edu/iu/c322/assetmanagement/licensingservice/model/Organization.java
+++
@@ -1,8 +0,0 @@
-package edu.iu.c322.assetmanagement.licensingservice.model;
-
-public record Organization(String id,
-        String name,
-        String contactName,
-        String contactEmail,
-        String contactPhone) {
-}
--- src/main/java/edu/iu/c322/assetmanagement/licensingservice/repository/AssetRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.iu.c322.assetmanagement.licensingservice.repository;
-
-import edu.iu.c322.assetmanagement.licensingservice.model.Asset;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AssetRepository extends JpaRepository<Asset, Integer> {
-}
